57c80f6a8103047df4c99cc8cd6f500fc7409234
bkitano <briankitano@gmail.com>
2023-07-24 14:50:05 -0700

    setting up for pypi

--- setup.py
+++ setup.py
@@ -2,16 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="michina",
-    version="0.1",
+    version="0.0.1",
     packages=find_packages(),
-    install_requires=[
-        "langchain",
-        "python-dotenv",
-        "pydantic",
-    ],
-    entry_points={
-        "console_scripts": [
-            "michina=michina.michina:main",
-        ],
-    },
 )
